025340c224507b99b0cd2d67ef65b38d614ed11e
Nills Maillet <mailletnills@gmail.com>
2026-09-09 15:47:55 +0200

    refactor: change number attribute to street & france/germany adresses objects created

--- src/Addr.java
+++ src/Addr.java
@@ -1,12 +1,12 @@
 public class Addr {
 
-    public int number;
+    public String street;
     public String postal;
     public String cityName;
     public String countryName;
 
-    public Addr(int number, String postal, String cityName, String countryName) {
-        this.number = number;
+    public Addr(String street, String postal, String cityName, String countryName) {
+        this.street = street;
         this.postal = postal;
         this.cityName = cityName;
         this.countryName = countryName;
--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,7 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
 
+    Addr franceAdress = new Addr("37 Avenue de la Capucine", "31100", "Toulouse", "France");
+    Addr germanyAdress = new Addr("9 Kleiststr.", "52477", "Alsdorf", "Germany");
+
 }
